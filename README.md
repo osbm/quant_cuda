43254f2bef1868050daf27488f64efc9dfe70e87
osbm <osmanfbayram@gmail.com>
2024-05-05 14:38:39 +0300

    add package

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # quant_cuda
 quant cuda cuda kernel python package
+
+this is a bit cursed way of doing this but 
+
+```
+pip install setuptools torch
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+from torch.utils import cpp_extension
+
+setup(
+    name='quant_cuda',
+    setup_requires=["torch"],
+    ext_modules=[cpp_extension.CUDAExtension(
+        'quant_cuda', 
+        [
+        	'quant_cuda/quant_cuda.cpp', 
+        	'quant_cuda/quant_cuda_kernel.cu'
+        ]
+    )],
+    cmdclass={'build_ext': cpp_extension.BuildExtension}
+)
